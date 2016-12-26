a7fcf9267fc93f6bf245a06f4d11f11a4d2ba8a3
Jason Humphreys <jasonphumphreys@gmail.com>
2016-12-26 15:09:25 -0600

    thru video 15

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import Fish from './Fish';
 import sampleFishes from '../sample-fishes'
 class App extends React.Component {
   constructor () {
@@ -22,7 +23,7 @@ class App extends React.Component {
     const timestamp = Date.now();
     fishes[`fish-${timestamp}`] = fish;
     //set state
-    this.setState({ fishes });
+    this.setState({ fishes: fishes });
   }
   loadSamples () {
     this.setState({
@@ -34,6 +35,13 @@ class App extends React.Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="list-of-fishes">
+            {
+              Object
+              .keys(this.state.fishes)
+              .map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+            }
+          </ul>
         </div>
         <Order />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
