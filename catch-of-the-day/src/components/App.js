b0fd5ecc1fb2ca2c4d79444f2b24e8cf55985d2c
Jason Humphreys <jasonphumphreys@gmail.com>
2016-12-27 13:44:30 -0600

    thru video 21

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -12,9 +12,10 @@ class App extends React.Component {
 
     this.addFish = this.addFish.bind(this);
     this.updateFish = this.updateFish.bind(this);
+    this.removeFish = this.removeFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
-    this.addToOrder = this.addToOrder.bind(this);
+    this.removeFromOrder = this.removeFromOrder.bind(this);
     //getInitialState
     this.state = {
       fishes: {},
@@ -65,6 +66,12 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  removeFish(key) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = null;
+    this.setState({ fishes })
+  }
+
   loadSamples () {
     this.setState({
       fishes: sampleFishes
@@ -79,6 +86,13 @@ class App extends React.Component {
     //update our state
     this.setState({ order });
   }
+
+  removeFromOrder(key) {
+    const order = {...this.state.order};
+    delete order[key];
+    this.setState({ order })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -88,7 +102,8 @@ class App extends React.Component {
             {
               Object
               .keys(this.state.fishes)
-              .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
+              .map(key => <Fish key={key} index={key} details={this.state.fishes[key]}
+                addToOrder={this.addToOrder} />)
             }
           </ul>
         </div>
@@ -96,9 +111,11 @@ class App extends React.Component {
           fishes={this.state.fishes}
           order={this.state.order}
           params={this.props.params}
+          removeFromOrder={this.removeFromOrder}
           />
         <Inventory
           addFish={this.addFish}
+          removeFish={this.removeFish}
           loadSamples={this.loadSamples}
           fishes={this.state.fishes}
           updateFish={this.updateFish}
