d83b4f6a14a3d4ead68069d36e2a5ae62b822340
Jason Humphreys <jasonphumphreys@gmail.com>
2016-12-27 11:39:26 -0600

    thru video 18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBSkf47u6SEqcp4SzvoHWJYmUXxgNlPsDI",
+  authDomain: "react-wes-bos-6b71c.firebaseapp.com",
+  databaseURL: "https://react-wes-bos-6b71c.firebaseio.com"
+})
+
+export default base
